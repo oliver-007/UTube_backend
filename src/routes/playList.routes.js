888a36d0ae37fcce4211e1d7626746466c9ad4a5
oliver-007 <oliverahmed000@gmail.com>
2024-07-29 01:32:12 +0600

    add getAnyUsersAllPlaylist route

--- src/routes/playList.routes.js
+++ src/routes/playList.routes.js
@@ -3,7 +3,8 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 import {
   addVideoToPlaylist,
   createPlaylist,
-  getUserAllPlaylists,
+  deletePlaylist,
+  getAnyUsersAllPlaylist,
   removeVideoFromPlaylist,
   updatePlaylist,
 } from "../controllers/playlist.controller.js";
@@ -14,16 +15,16 @@ router.use(verifyJwt); // USER AUTHENTICATION WILL APPLY FOR ALL ROUTES.
 // +++++ CREATE PLAYLIST ROUTE +++++
 router.route("/").post(createPlaylist);
 
-// ++++++ UPDATE PLAYLIST NAME & DETAILS ROUTE +++++++
-router.route("/:playlistId").patch(updatePlaylist);
-
-// +++++++++ GET ANY USER'S ALL PLAYLISTS ROUTE ++++++++++
-router.route("/u/:userId").get(getUserAllPlaylists);
+// ++++++ UPDATE PLAYLIST NAME & DETAILS + DELETE PLAYLIST ROUTE +++++++
+router.route("/:playlistId").patch(updatePlaylist).delete(deletePlaylist);
 
 // +++++ ADD VIDEO TO PLAYLIST ROUTE ++++++
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 
 // +++++++ REMOVE VIDEO FROM PLAYLIST ROUTE ++++++
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+
+// +++++++++ GET ANY USER'S ALL PLAYLIST ROUTE ++++++++++
+router.route("/u/:userId").get(getAnyUsersAllPlaylist);
 
 export default router;
